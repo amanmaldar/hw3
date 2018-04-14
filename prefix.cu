e552499cb87584d8bf5ea12e75eb1af672115da1
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 18:14:26 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -9,11 +9,12 @@ using namespace std;
 __global__ void vec_mult_kernel (int *a, int n) {
 int tid = blockIdx.x; // initialize with block number. Tid = 0 -> 10240
 __shared__ int smem[256];
-smem[threadIdx.x] = b[threadIdx.x];
+smem[threadIdx.x] = a[threadIdx.x];
 __syncthreads(); //wait for all threads
 while (tid < n) {
 //smem[threadIdx.x] *= a[tid*N + threadIdx.x];
 smem[threadIdx.x] *= a[threadIdx.x] * 2;
+  a[threadIdx.x] = smem[threadIdx.x];
 __syncthreads();
 tid += 128; // Jump to next block which is away by 128 blocks w.r.t. current one
 } // end while
@@ -26,7 +27,9 @@ main (int args, char **argv)
 int n = 8;
 int *a= (int *)malloc(sizeof(int)*n);
 // Initialize matrix A and B
-for (int i = 0; i < n; i++) { a[i] = rand () % 5 + 2; }
+  cout << "array is: ";
+for (int i = 0; i < n; i++) { a[i] = rand () % 5 + 2; cout << a[i] << " ";}
+  cout << endl;
 int *a_d; //device storage pointers
 
 cudaMalloc ((void **) &a_d, sizeof (int) * n);
@@ -34,8 +37,11 @@ cudaMemcpy (a_d, a, sizeof (int) * n, cudaMemcpyHostToDevice);
 
 // perform multiplication on GPU
 auto time_beg = wtime();
-//vec_mult_kernel <<< 128,256 >>> (a_d, n );
+vec_mult_kernel <<< 128,256 >>> (a_d, n );
 cudaMemcpy (a, a_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
+  cout << "result is: ";
+for (int i = 0; i < n; i++) {  cout << a[i] << " ";}
+  cout << endl;
 auto el = wtime() - time_beg;
 cout << "Time for <128,256> is: " << el << " Sec " << endl;
 return 0;
