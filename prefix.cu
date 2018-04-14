18222b0a88bfc51cb9a92ab2f50c5bb80f76710f
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 17:54:34 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -11,29 +11,7 @@ using namespace std;
 // rows = M = 10240, columns = N = 256, vector b[256].  1 block operates on 1 row. Each block has 256 threads.
 // Number of blocks = 128, so increment tid by 128.
 __global__ void prefix_scan (int *x_d, int n) { 
-  	//int tid = threadIdx.x + blockIdx.x * blockDim.x;		// initialize with block number. Tid = 0 -> 10240
- 	int tid = threadIdx.x ;
-	//__shared__ has scope of block. All threads in block has access to it.
- 	__shared__ int smem[8];   
-	//smem[threadIdx.x] = x_d[threadIdx.x];
- 	//smem[tid] = x_d[tid];
- 	//__syncthreads(); 	//wait for all threads to copy data to smem
-
-	while (tid < 8) { 
-		
-	/*	if (tid == 0){
-			smem[tid] = smem[tid+1];
-			x_d[tid] = smem[tid];
-		}
-		else {
-			smem[tid] += smem[tid-1];
-			x_d[tid] = smem[tid];
-			__syncthreads();
-		}	 */
-		//x_d[tid] = smem[tid];
-  	  
-		tid += 8;	// Jump to next block which is away by 128 blocks w.r.t. current one
-  	}	
+  	
 
 } // end kernel function
 
