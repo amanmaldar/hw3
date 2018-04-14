0ee0e0a39ee8814ad05f322f3088424bdb296634
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 18:05:38 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -4,54 +4,57 @@
 #include <assert.h>
 #include <iostream>
 #include <chrono>
-#include "helper/wtime.h"
-#include "helper/util.h"
-#include <assert.h>
+#include "../helper/wtime.h"
 using namespace std;
-
-
-// rows = M = 10240, columns = N = 256, vector b[256].  1 block operates on 1 row. Each block has 256 threads.
-// Number of blocks = 128, so increment tid by 128.
-__global__ void prefix_scan (int *x_d, int n) { 
-  	
-
+__global__ void vec_mult_kernel (int *a, int *b, int *c, int M, int N) {
+int tid = blockIdx.x; // initialize with block number. Tid = 0 -> 10240
+__shared__ int smem[256];
+smem[threadIdx.x] = b[threadIdx.x];
+__syncthreads(); //wait for all threads
+while (tid < M) {
+smem[threadIdx.x] *= a[tid*N + threadIdx.x];
+__syncthreads();
+// Threads should also perform parallel reduction to sum up smem[256] vector
+// size is also reduced to half after every for loop execution.
+for (int i= blockDim.x/2; i>0; i = i/2) {
+if(threadIdx.x < i) {
+int temp = smem[threadIdx.x] + smem[threadIdx.x + i];
+smem[threadIdx.x] = temp;
+}
+} // for ends
+// once parallel reduction is complete, result is at smem[0]
+if (threadIdx.x == 0) {
+c[tid] = smem[0];
+}
+tid += 128; // Jump to next block which is away by 128 blocks w.r.t. current one
+} // end while
 } // end kernel function
-
 
 int
 main (int args, char **argv)
 {
-  int n;
-  //n = 32000000;
-  n = 8;	   
-  int *x= (int *)malloc(sizeof(int)*n);
-	
-  cout << "original array: ";
-  for (int i = 0; i < n; i++) {     
-	  x[i] = rand () % 5 + 2;  
-	  cout << x[i] << " ";       
-  }   cout << endl;
-	
-  int *x_d;	//device storage pointers 
-
-	
-  H_ERR(cudaMalloc ((void **) &x_d, sizeof (int)));
-	//cudaMalloc ((void **) &a_d, sizeof (int) * M*N);
-  //H_ERR(cudaMemcpy (x_d, x, sizeof (int) * n, cudaMemcpyHostToDevice));
-  
-  // perform prefix_scan on GPU
- // auto time_beg = wtime();  
- // prefix_scan <<< 128,128 >>> (x_d,n);
-	cout << "done " ;
- // H_ERR(cudaMemcpy (x, x_d, sizeof (int) * n, cudaMemcpyDeviceToHost));
-  //auto el = wtime() - time_beg;
- // cout << "Time for <128,128> is: " << el << " Sec " << endl;
-
-  cout << "result is: " ;
-	for (int i = 0; i < n; i++){
-		cout << x_d[i] << " ";
-	}
-	cout << endl;
-	
-    return 0;
+// configure matrix dimensions
+int M,N,P;
+M = 10240;
+N = 256;
+P = 1;
+int *a= (int *)malloc(sizeof(int)*M*N);
+int *b= (int *)malloc(sizeof(int)*N*P);
+int *c= (int *)malloc(sizeof(long)*M*P);
+// Initialize matrix A and B
+for (int i = 0; i < M*N; i++) { a[i] = rand () % 5; }
+for (int i = 0; i < N*P; i++) { b[i] = rand () % 5; }
+int *a_d, *b_d, *c_d; //device storage pointers
+cudaMalloc ((void **) &a_d, sizeof (int) * M*N);
+cudaMalloc ((void **) &b_d, sizeof (int) * N*P);
+cudaMalloc ((void **) &c_d, sizeof (int) * M*P);
+cudaMemcpy (a_d, a, sizeof (int) * M*N, cudaMemcpyHostToDevice);
+cudaMemcpy (b_d, b, sizeof (int) * N*P, cudaMemcpyHostToDevice);
+// perform multiplication on GPU
+time_beg = wtime();
+vec_mult_kernel <<< 128,256 >>> (a_d, b_d, c_d, M, N,P );
+cudaMemcpy (c, c_d, sizeof (int) * M*P, cudaMemcpyDeviceToHost);
+el = wtime() - time_beg;
+cout << "Time for <128,256> is: " << el << " Sec " << endl;
+return 0;
 }
