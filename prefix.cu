f243223a999365f0bbb13acecdc94ac669456870
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 19:52:50 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -13,8 +13,8 @@ smem[threadIdx.x] = a[threadIdx.x];
 __syncthreads(); //wait for all threads
 while (tid < n) {
   if (tid == 0) { smem[0] = a[0]; a[threadIdx.x] = smem[threadIdx.x];  break;}
-//smem[threadIdx.x] *= a[tid*N + threadIdx.x];
-//smem[threadIdx.x] += smem[threadIdx.x - 1] ;
+
+  for (int depth = 0; depth < 3; depth++)
 
   smem[threadIdx.x] += a[threadIdx.x-1] ;
   a[threadIdx.x] = smem[threadIdx.x];
@@ -23,27 +23,58 @@ tid += 128; // Jump to next block which is away by 128 blocks w.r.t. current one
 } // end while
 } // end kernel function
 
+
+
+
+__global__ void scan(int *g_odata, int *g_idata, int n)
+{
+ extern __shared__ float temp[]; // allocated on invocation
+ int thid = threadIdx.x;
+ int pout = 0, pin = 1;
+ // load input into shared memory.
+ // This is exclusive scan, so shift right by one and set first elt to 0
+ temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
+ __syncthreads();
+ for (int offset = 1; offset < n; offset *= 2)
+ {
+ pout = 1 - pout; // swap double buffer indices
+ pin = 1 - pout;
+ if (thid >= offset)
+ temp[pout*n+thid] += temp[pin*n+thid - offset];
+ else
+ temp[pout*n+thid] = temp[pin*n+thid];
+ __syncthreads();
+ }
+ g_odata[thid] = temp[pout*n+thid1]; // write output
+} 
+
+
+
 int
 main (int args, char **argv)
 {
 // configure matrix dimensions
 int n = 8;
 int *a= (int *)malloc(sizeof(int)*n);
+int *b= (int *)malloc(sizeof(int)*n);
 // Initialize matrix A and B
   cout << "array is: ";
 for (int i = 0; i < n; i++) { a[i] = rand () % 5 + 2; cout << a[i] << " ";}
   cout << endl;
-int *a_d; //device storage pointers
+int *a_d, *b_d; //device storage pointers
 
 cudaMalloc ((void **) &a_d, sizeof (int) * n);
+cudaMalloc ((void **) &b_d, sizeof (int) * n);
+
 cudaMemcpy (a_d, a, sizeof (int) * n, cudaMemcpyHostToDevice);
 
 // perform multiplication on GPU
 auto time_beg = wtime();
-vec_mult_kernel <<< 128,256 >>> (a_d, n );
-cudaMemcpy (a, a_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
+//vec_mult_kernel <<< 128,256 >>> (a_d, n );
+void scan(int *b_d, int *a_d, int n)
+cudaMemcpy (b, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
   cout << "result is: ";
-for (int i = 0; i < n; i++) {  cout << a[i] << " ";}
+for (int i = 0; i < n; i++) {  cout << b[i] << " ";}
   cout << endl;
 auto el = wtime() - time_beg;
 cout << "Time for <128,256> is: " << el << " Sec " << endl;
