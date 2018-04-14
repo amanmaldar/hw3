2783589f7d3e4739f1cec1599d5e4cda6b9bcc97
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 18:11:27 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -6,26 +6,15 @@
 #include <chrono>
 #include "helper/wtime.h"
 using namespace std;
-__global__ void vec_mult_kernel (int *a, int *b, int *c, int M, int N) {
+__global__ void vec_mult_kernel (int *a, int n) {
 int tid = blockIdx.x; // initialize with block number. Tid = 0 -> 10240
 __shared__ int smem[256];
 smem[threadIdx.x] = b[threadIdx.x];
 __syncthreads(); //wait for all threads
-while (tid < M) {
-smem[threadIdx.x] *= a[tid*N + threadIdx.x];
+while (tid < n) {
+//smem[threadIdx.x] *= a[tid*N + threadIdx.x];
+smem[threadIdx.x] *= a[threadIdx.x] * 2;
 __syncthreads();
-// Threads should also perform parallel reduction to sum up smem[256] vector
-// size is also reduced to half after every for loop execution.
-for (int i= blockDim.x/2; i>0; i = i/2) {
-if(threadIdx.x < i) {
-int temp = smem[threadIdx.x] + smem[threadIdx.x + i];
-smem[threadIdx.x] = temp;
-}
-} // for ends
-// once parallel reduction is complete, result is at smem[0]
-if (threadIdx.x == 0) {
-c[tid] = smem[0];
-}
 tid += 128; // Jump to next block which is away by 128 blocks w.r.t. current one
 } // end while
 } // end kernel function
