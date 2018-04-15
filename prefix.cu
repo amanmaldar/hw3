d64e8107f7a6066edd99b4d583b32d558242edd6
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-15 11:05:27 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -60,7 +60,7 @@ cudaMemcpy (a_d, a, sizeof (int) * n, cudaMemcpyHostToDevice);
 
 // perform multiplication on GPU
 auto time_beg = wtime();
-vec_mult_kernel <<< 8,8 >>> (b_d,a_d, n );
+vec_mult_kernel <<< 1,16 >>> (b_d,a_d, n );
 cudaMemcpy (b, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
   cout << "result is: ";
 for (int i = 0; i < n; i++) {  cout << b[i] << " ";}
