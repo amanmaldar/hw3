debd9466d321fb2da5c15372f91c5efa68ff4eee
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-15 15:10:05 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -60,7 +60,7 @@ main (int args, char **argv)
   int threadsInBlock = 128;
   int numberOfBlocks = 2;
   //int n = threadsInBlock*numberOfBlocks;
-  int n = 128;
+  int n = 256;
   int b_cpu[n];
   int depth = log2(threadsInBlock);    //log(blockDim.x) = log(8) = 3,  blockDim.x = threadsInBlock
 
