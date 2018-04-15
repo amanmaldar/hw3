0e7ea25058df4c6b443257b31fb670bc891e3e36
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-15 13:12:54 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -12,7 +12,7 @@ using namespace std;
 __global__ void vec_mult_kernel (int *b_d, int *a_d, int n) {
 int tid = blockIdx.x* blockDim.x+ threadIdx.x; // initialize with block number. Tid = 0 -> 10240
 __shared__ int smem[512];
-  int depth = 5;    //log(blockDim.x) = log(8) = 3
+  int depth = 9;    //log(blockDim.x) = log(8) = 3
   int d =0;
   int offset = 0;
   
@@ -56,7 +56,7 @@ void fillPrefixSum(int arr[], int n, int prefixSum[])
 int
 main (int args, char **argv)
 {
-  int n = 32;
+  int n = 512;
   int b_cpu[n];
 
   int *a= (int *)malloc(sizeof(int)*n);
@@ -82,7 +82,7 @@ main (int args, char **argv)
   cudaMemcpy (a_d, a, sizeof (int) * n, cudaMemcpyHostToDevice);
 
   time_beg = wtime();
-  vec_mult_kernel <<< 1,32 >>> (b_d,a_d, n );
+  vec_mult_kernel <<< 1,512 >>> (b_d,a_d, n );
   cudaMemcpy (b, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
   auto el_gpu = wtime() - time_beg;
 
