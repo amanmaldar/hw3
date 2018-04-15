a73ada6b3a24985406f152dbc043747d743e9150
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-15 15:37:49 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -27,14 +27,14 @@ __shared__ int smem[128];    // numberOfBlocks*threadsInBlock  = 2^7 + 2^7 = 16K
   int d = 0;
   int offset = 0;
   
-    smem[threadIdx.x] = a_d[blockIdx.x*128+threadIdx.x];   // copy data to shared memory
+    smem[threadIdx.x] = a_d[tid];   // copy data to shared memory
   __syncthreads(); //wait for all threads
     
   while (tid < n) {
   
 
   if (tid%128 == 0 ) { smem[0] = smem[0] + res;   //** add previos result to telement zero
-                              b_d[blockIdx.x*128] = smem[0]; break;}  
+                              b_d[blockIdx.x*128] = smem[0]; tid += 128; break;}  
 
   offset = 1; //1->2->4
   for (d =0; d < 7 ; d++){                        // depth = 3
