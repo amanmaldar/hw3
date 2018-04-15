03cc583478960193912873af962dd63dde786793
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-14 21:03:56 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -20,8 +20,8 @@ while (tid < n) {
   if (tid == 0) { smem[0] = a_d[0]; b_d[0] = smem[0]; tid += 8; break;}
   //if (tid == 1) { smem[1] = 1001; b_d[1] = smem[0]; tid += 8; break;}
   for (d =0; d < 1; ++d){
-    offset = 2^d;
-    if (tid >= offset){
+    offset = 1; //2^d;
+    if (tid >0){
         //smem[threadIdx.x] += smem[threadIdx.x-1] ;
       __syncthreads();
       smem[tid] += smem[tid-1] ;
