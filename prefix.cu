6e86c12ec4f9005f04f2deb2050051ac3704d389
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-16 17:25:56 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -90,9 +90,9 @@ main (int args, char **argv)
 
   time_beg = wtime();
   vec_mult_kernel <<< numberOfBlocks,threadsInBlock >>> (b_d,a_d, n, depth );
-          auto el_gpu = wtime() - time_beg;
-
   cudaMemcpy (b_cpu, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
+              auto el_gpu = wtime() - time_beg;
+
 
     // cpu combines the results of each block with next block. cpu basically adds last element from previos block to
     // next element in next block. This is sequential process.
