e0c2bd127507f176e43967977437e0967f75da15
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-16 16:25:39 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -17,13 +17,12 @@ void fillPrefixSum(int arr[], int n, int prefixSum[])
 }
 
 __device__ int res=0;  //result from one block to next block
-__device__ int smem[256];
+__device__ int smem[16384]; // 128*128
 
 __global__ void vec_mult_kernel (int *b_d, int *a_d, int n, int depth) {
   
 int tid = blockIdx.x* blockDim.x+ threadIdx.x; 
- // int smemSize = blockDim.x*gridDim.x;
-//__shared__ int smem[128];    // numberOfBlocks*threadsInBlock  = 2^7 + 2^7 = 16K shared memory
+
   int d = 0;
   int offset = 0;
     
@@ -31,11 +30,10 @@ int tid = blockIdx.x* blockDim.x+ threadIdx.x;
       smem[tid] = a_d[tid];   // copy data to shared memory
   __syncthreads(); //wait for all threads
 
-  if (tid%128 == 0 ) { smem[0] = smem[0] + res;   //** add previos result to telement zero
-                              b_d[blockIdx.x*128] = smem[0]; }  
+  if (threadIdx.x == 0 ) {   b_d[tid] = smem[tid]; }  
 
   offset = 1; //1->2->4
-  for (d =0; d < 7 ; d++){                        // depth = 3
+  for (d =0; d < depth ; d++){                        // depth = 3
     
     if (tid%blockDim.x >= offset){  
      
@@ -45,9 +43,9 @@ int tid = blockIdx.x* blockDim.x+ threadIdx.x;
     }// end if
     offset *=2;
    } // end for 
-   b_d[tid] = smem[threadIdx.x]; //+ res; no need as we alreasy are adding result to element zero above **  // save result to b_d after adding res to it;
+   b_d[tid] = smem[tid]; //+ res; no need as we alreasy are adding result to element zero above **  // save result to b_d after adding res to it;
       __syncthreads();
-  if(tid == 127) {res = smem[127];     }  // if last thread in block save cout
+  
   tid += gridDim.x*blockDim.x;
 
 } // end while (tid < n)
@@ -57,28 +55,29 @@ int tid = blockIdx.x* blockDim.x+ threadIdx.x;
 int
 main (int args, char **argv)
 {
-  int threadsInBlock = 128;
-  int numberOfBlocks = 2;
-  //int n = threadsInBlock*numberOfBlocks;
-  int n = 32000000;
+  int threadsInBlock = 8;
+  int numberOfBlocks = 8;
+  int n = threadsInBlock*numberOfBlocks;
+  //int n = 32000000;
   //int b_cpu[n];
   int depth = log2(threadsInBlock);    //log(blockDim.x) = log(8) = 3,  blockDim.x = threadsInBlock
 
-  int *a= (int *)malloc(sizeof(int)*n);
+  int *a_cpu= (int *)malloc(sizeof(int)*n);
   int *b_cpu= (int *)malloc(sizeof(int)*n);
-  
+  int *b_ref= (int *)malloc(sizeof(int)*n);
+    
   cout << "\n array is: "; 
-  for (int i = 0; i < n; i++) { a[i] = rand () % 5 + 2; //cout << a[i] << " ";
+  for (int i = 0; i < n; i++) { a_cpu[i] = rand () % 5 + 2; cout << a_cpu[i] << " ";
                               }   cout << endl;
   
   auto time_beg = wtime();
-  fillPrefixSum(a, n, b_cpu);
+  fillPrefixSum(a_cpu, n, b_ref);
   auto el_cpu = wtime() - time_beg;
-  cout << "CPU time is: " << el_cpu * 1000 << " mSec " << endl;
-/*   
+  //cout << "CPU time is: " << el_cpu * 1000 << " mSec " << endl;
+  
   cout << "\n CPU Result is: "; 
   for (int i = 0; i < n; i++) 
-  { //cout << b_cpu[i] << " ";   
+  { cout << b_ref[i] << " ";   
   } cout << endl;
   
   int *a_d, *b_d; //device storage pointers
@@ -86,18 +85,18 @@ main (int args, char **argv)
   cudaMalloc ((void **) &a_d, sizeof (int) * n);
   cudaMalloc ((void **) &b_d, sizeof (int) * n);
 
-  cudaMemcpy (a_d, a, sizeof (int) * n, cudaMemcpyHostToDevice);
+  cudaMemcpy (a_d, a_cpu, sizeof (int) * n, cudaMemcpyHostToDevice);
 
   time_beg = wtime();
   vec_mult_kernel <<< numberOfBlocks,threadsInBlock >>> (b_d,a_d, n, depth );
   auto el_gpu = wtime() - time_beg;
-  cudaMemcpy (b, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
+  cudaMemcpy (b_cpu, b_d, sizeof (int) * n, cudaMemcpyDeviceToHost);
 
 
   cout << "\n GPU Result is: ";
   for (int i = 0; i < n; i++) {    
-    //assert(b[i]== b_cpu[i]);   
-    cout << b[i] << " ";  
+    //assert(b_ref[i]== b_cpu[i]);   
+    cout << b_cpu[i] << " ";  
   } cout << endl;
 
   cout << "CPU time is: " << el_cpu * 1000 << " mSec " << endl;
