9c3289c86904406251b8b08156bc1f49a71e7ce7
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-17 00:02:56 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -73,7 +73,7 @@ main (int args, char **argv)
   int threadsInBlock = 128;
   int numberOfBlocks = 128;
   //int n = threadsInBlock*numberOfBlocks;
-  int n = 16384;
+  int n = 32000000;
   int depth = log2(16384);  
 
   int *a_cpu= (int *)malloc(sizeof(int)*n);
