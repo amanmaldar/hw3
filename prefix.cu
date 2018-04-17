392a06641996a9d73bfd74d8c791a562da856316
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-16 20:11:30 -0400

    Update prefix.cu

--- prefix.cu
+++ prefix.cu
@@ -69,9 +69,10 @@ int tid = blockIdx.x* blockDim.x+ threadIdx.x;
       __syncthreads();
      // 3 new line below
      
-      if (blockIdx.x != '0' && threadIdx.x == '0') 
+      //if (blockIdx.x != 0 && threadIdx.x == 0) 
+          if ( threadIdx.x == 0) 
       {
-          tmp1 = smem[tid-1]; __syncthreads();
+          tmp1 = 500;//smem[tid-1]; __syncthreads();
           smem[tid] += 1000; __syncthreads();
       }
       else if( blockIdx.x != 0 & threadIdx.x > 0 & threadIdx.x < 4)
